8ac78cdb59aff8c92f946233e79b95ca842dc5c8
Alexander Kessaris <ank352@nyu.edu>
2021-07-15 10:20:18 +0100

    css changes

--- src/components/NavBar/NavBar.component.js
+++ src/components/NavBar/NavBar.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
@@ -14,13 +13,14 @@ export default function NavBar() {
 
   return (
     <BrowserRouter>
-      <Paper>
+      <div>
         <Tabs
             value={value}
             indicatorColor="primary"
             textColor="primary"
             onChange={handleChange}
             aria-label="Navigation Bar"
+            style={{ marginBottom: '20px' }}
           >
           <Tab key="Pokedex" label="Pokedex" value="Pokedex" component={Link} to="/pokemon"/>
 
@@ -30,7 +30,7 @@ export default function NavBar() {
           <Route path="/" exact><Redirect to="/pokemon" /></Route>
           <Route path="/pokemon" exact><PokemonList /></Route>
         </Switch>
-      </Paper>
+      </div>
     </BrowserRouter>
 
   );
