28d1268a2e6f7b7a25f31084a47eeeffd2674319
Alexander Kessaris <ank352@nyu.edu>
2021-07-16 11:01:02 +0100

    added query for fetching single pokemon

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,4 +1,19 @@
-const getQuery = (num) => {
+const getPokemonQuery = (name) => {
+  return `query pokemon {
+    pokemon (name: "${name}") {
+      number
+      name
+      types
+      image
+      evolutions {
+        name
+        image
+      }
+    }
+  }`;
+};
+
+const getPokemonsQuery = (num) => {
   return `query pokemons {
     pokemons(first: ${num}) {
       number
@@ -11,14 +26,25 @@ const getQuery = (num) => {
 }
 
 export const fetchPokemon = async (num) => {
+  const query = getPokemonQuery(num);
+  const { data: { pokemon } } = await fetchData(query);
+  return pokemon;
+};
+
+export const fetchPokemons = async (num) => {
+  const query = getPokemonsQuery(num);
+  const { data: { pokemons } } = await fetchData(query);
+  return pokemons;
+};
+
+const fetchData = async (query) => {
   return fetch('https://graphql-pokemon2.vercel.app/', {
     credentials: 'omit',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      query: getQuery(num),
+      query
     }),
     method: 'POST',
   })
-    .then((res) => res.json())
-    .then(({ data: { pokemons } } ) => pokemons);
-}
+  .then((res) => res.json());
+};
